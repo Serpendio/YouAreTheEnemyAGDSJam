81d09a254fd6ef071c0c01202c13db430ca8f30f
Serpendio <58090288+Serpendio@users.noreply.github.com>
2023-09-29 02:51:40 +0100

    Can "win" & "lose" now

---
+++ YouAreTheEnemyAGDS/Assets/Scripts/Interactions/OutOfBounds.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+            print("Ghost Won");
+    }
+}
