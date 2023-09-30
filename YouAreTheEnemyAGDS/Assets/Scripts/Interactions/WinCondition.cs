a3df9806e9d8a3180165dfbc4beb5462855916c0
Serpendio <58090288+Serpendio@users.noreply.github.com>
2023-09-30 05:35:45 +0100

    Updated a few interactable sprites
    Also added a shimmery effect :D

--- YouAreTheEnemyAGDS/Assets/Scripts/Interactions/WinCondition.cs
+++ YouAreTheEnemyAGDS/Assets/Scripts/Interactions/WinCondition.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour
 {
+    [SerializeField] Sprite unlockedSprite;
+
+    bool isLocked = true;
+
+    public void Unlock()
+    {
+        GetComponent<SpriteRenderer>().sprite = unlockedSprite;
+        isLocked = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isLocked)
             print("Escapist Won");
     }
 }
